e81277268836fd53d982ab3cef4bb58b79a0500a
ChristianCeT <chrisatnat@hotmail.com>
2022-08-18 17:57:47 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_test_ra/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_test_ra
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test_ra/ar_viewers/ar_debug_options_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ARDebugOptionsScreen(),
+    );
+  }
+}
